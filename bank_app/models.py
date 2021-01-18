44001863666bae5289e07c65ec14b1d9ab4a6246
abhi204 <abhi.chaurasia204@gmail.com>
2021-01-19 00:24:17 +0530

    Initialize bank_app with models

---
+++ bank_app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Bank(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    name = models.CharField(max_length=49)
+
+class Branch(models.Model):
+    ifsc = models.CharField(max_length=11, primary_key=True)
+    bank_id = models.ForeignKey('Bank', on_delete=models.CASCADE)
+    branch = models.CharField(max_length=74)
+    address = models.CharField(max_length=195)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=26)
