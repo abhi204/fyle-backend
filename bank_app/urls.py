36bc3e5379b7497a7b1097ca9b9ba7391a8b161a
abhi204 <abhi.chaurasia204@gmail.com>
2021-01-25 20:23:48 +0530

    Add field-search API endpoint

--- bank_app/urls.py
+++ bank_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from bank_app.views import AutoCompleteList, AllPossibleMatchList
+from bank_app.views import AutoCompleteList, AllPossibleMatchList, SearchByFieldMatchList
 
 urlpatterns = [
     path('branches', AllPossibleMatchList.as_view()),
     path('branches/autocomplete', AutoCompleteList.as_view()),
+    path('branches/fieldsearch', SearchByFieldMatchList.as_view()),
 ]
--- bank_app/views.py
+++ bank_app/views.py
@@ -1,16 +1,35 @@
 from bank_app.models import Branch
 from bank_app.serializers import BranchSerializer
+import django_filters 
+from django_filters.rest_framework import FilterSet, DjangoFilterBackend
 from rest_framework import filters, generics
 
 
 class AutoCompleteList(generics.ListAPIView):
     queryset = Branch.objects.all().order_by('ifsc')
     serializer_class = BranchSerializer
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [filters.SearchFilter]
     search_fields = ['^branch']
 
 class AllPossibleMatchList(generics.ListAPIView):
     queryset = Branch.objects.all().order_by('ifsc')
     serializer_class = BranchSerializer
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [filters.SearchFilter]
     search_fields = ['^ifsc', '^branch', '^address', '^city', '^district', '^state']
+
+class BranchFilter(FilterSet):
+    ifsc = django_filters.CharFilter(lookup_expr='istartswith')
+    branch = django_filters.CharFilter(lookup_expr='istartswith')
+    address = django_filters.CharFilter(lookup_expr='istartswith')
+    city = django_filters.CharFilter(lookup_expr='istartswith')
+    district = django_filters.CharFilter(lookup_expr='istartswith')
+    state = django_filters.CharFilter(lookup_expr='istartswith')
+    class Meta:
+        model = Branch
+        fields = ['ifsc', 'branch', 'address', 'city', 'district', 'state']
+
+class SearchByFieldMatchList(generics.ListAPIView):
+    queryset = Branch.objects.all().order_by('ifsc')
+    serializer_class = BranchSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BranchFilter
